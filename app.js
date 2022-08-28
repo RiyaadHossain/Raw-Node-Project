308465f5b5d40f961542a58bd8672e95ffcef113
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-08-28 10:44:12 +0600

    all task completed

--- app.js
+++ app.js
@@ -1,10 +1,55 @@
 const http = require("http")
+const fs = require("fs")
 const PORT = 5000
 
-const server = http.createServer((req, res) => {
+const server = http.createServer( (req, res) => {
+    
     if (req.url === "/") {
-        res.end("<p>Welcome to Full Stack Development </p>")
+        res.write("<p>Welcome to Full Stack Development </p>")
+        res.end()
     }
+
+    if (req.url === "/read") {
+        fs.readFile("./first.txt", (err, data) => {
+            if (err) {
+                res.write("Opps! Internal Server Error")
+                res.end()
+            } else {           
+                res.write(`<p>${data}</p>`)
+                res.end()
+            }
+        })
+    }
+
+    if (req.url === "/write") {
+        fs.readFile("first.txt", (err, data) => {
+            if (err) {
+                res.write("Opps! Internal Server Error")
+                res.end()
+            } else {
+                res.write(`<p>${ data }</p>`)
+                res.end()
+            }
+        })
+
+        fs.writeFile("second.txt", " I am a pull stack developer !!! 🤣", err => {
+            if (err) {
+                res.write("Opps! Internal Server Error")
+            } else {
+                res.write("Text written successfully")
+            }
+            res.end()
+        })
+    }
+
+    if (req.url === "/append") {
+       
+    }
+
+    if (req.url === "/delete") {
+      
+    }
+
 })
 
 server.listen(PORT)
